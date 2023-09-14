43bc5df7e2c92eacaa082a8e393e1ee4826ddf48
forhadhossen1 <mr.forhad1030@gamil.com>
2023-09-15 02:24:11 +0600

    Warnings are shown when multiple selection buttons are clicked. and set remaining time and credit time

--- src/components/bookmark/Bookmark.jsx
+++ src/components/bookmark/Bookmark.jsx
@@ -1,19 +1,23 @@
 import PropTypes from 'prop-types'
-const Bookmark = ({selectedCourse}) => {
+const Bookmark = ({ selectedCourse, totalHours, remaining }) => {
     return (
-        <div>
-            <h1 className="text-3xl">Course Name</h1>
+        <div className='bg-slate-300 rounded-lg p-5 mt-4 md:mt-0' >
+            <h1 className="text-xl font-semibold border-b-2 pb-4">Credit Hour Remaining {remaining} hrs</h1>
+            <h1 className="text-2xl font-semibold pt-4">Course Name</h1>
             {
                 selectedCourse.map(course => (
-                    <li key={course.id}> {course.course_title} </li>
+                    <li className=' p-2 ml-4 list-decimal font-medium' key={course.id}> {course.course_title} </li>
                 ))
             }
+
+            <h1 className="text-xl font-medium border-t-2 pt-4 mt-3">Total Credit Hour : {totalHours}</h1>
         </div>
     );
 };
 
 Bookmark.propTypes = {
-    selectedCourse: PropTypes.array
+    selectedCourse: PropTypes.array,
+
 }
 
 export default Bookmark;
--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -1,11 +1,14 @@
 /* eslint-disable react/jsx-key */
 import { useEffect, useState } from "react";
 import Bookmark from "../bookmark/Bookmark";
+import Swal from "sweetalert2";
 
 const Cards = () => {
 
     const [allCourse, setAllCourse] = useState([]);
     const [selectedCourse, setSelectedCourse] = useState([]);
+    const [totalHours, setTotalHours] = useState(0);
+    const [remaining, setRemaining] = useState(0);
 
     useEffect(() => {
         fetch('./course.json')
@@ -13,39 +16,60 @@ const Cards = () => {
             .then(data => setAllCourse(data))
     }, []);
 
+    const remainingTime = 20;
+
     const handleRegistration = (course) => {
-        setSelectedCourse([...selectedCourse, course]);
+        const isSelected = selectedCourse.find(item => item.id == course.id);
+
+        let hours = course.course_hours;
+        if (isSelected) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Already Selected This Course'
+              })
+        } else {
+            selectedCourse.forEach(item => {
+                hours = hours + item.course_hours;
+            });
+            const totalRemaining = remainingTime - hours;
+            setTotalHours(hours);
+            setRemaining(totalRemaining);
+
+            setSelectedCourse([...selectedCourse, course]);
+        }
     };
-    console.log(selectedCourse)
+    // console.log(selectedCourse)
 
 
 
     return (
-        <div className=" py-8 md:flex">
-            <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:w-2/3">
+        <div className=" py-8 md:flex gap-4">
+            <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:w-2/3 p-2">
                 {
                     allCourse.map(course => (
-                        <div key={course.id} className="card card-compact bg-base-100 shadow-xl">
+                        <div key={course.id} className="card card-compact bg-gray-300 shadow-xl">
                             <figure><img className="p-3 h-64 w-96" src={course.img} alt="title image" /></figure>
                             <div className="card-body">
                                 <h2 className="card-title text-2xl">{course.course_title}</h2>
                                 <p className="text-xl">{course.course_description}</p>
-                                <div className="flex justify-between text-xl font-medium py-5">
+                                <div className="flex justify-between text-lg py-5">
                                     <p><i className="fa-solid fa-dollar-sign"></i> Price : {course.price} $</p>
                                     <p><i className="fa-solid fa-book-open"></i> Credit : {course.course_hours} hrs</p>
                                 </div>
 
                                 <div className="card-actions">
-                                    <button onClick={()=>handleRegistration(course)} className="btn btn-primary w-full">select</button>
+                                    <button onClick={() => handleRegistration(course)} className="btn btn-primary w-full">select</button>
                                 </div>
                             </div>
                         </div>
                     ))
                 }
             </div>
-            <div>
-                <Bookmark selectedCourse ={selectedCourse}></Bookmark>
+            <div className="md:ml-4 p-2">
+                <Bookmark selectedCourse={selectedCourse} totalHours={totalHours} remaining={remaining}></Bookmark>
             </div>
+
         </div>
     );
 };
